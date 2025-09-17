a948064ff8f6c77df503e8138d58a92f9da5ccc0
Maria Guadalupe Delgadillo Avila <maria.delgadilloa@uanl.edu.mx>
2025-09-16 20:16:37 -0600

    Avanze para calificaciones

---
+++ Codigo.Calificaciones.py
@@ -0,0 +1,68 @@
+class Alumno:
+    def __init__(self,Nombre,Materia,Calificaciones):
+        self.Nombre = Nombre
+        self.Materia = Materia
+        self.Calificaciones = Calificaciones
+
+    def promedio(self):
+        return sum(self.Calificaciones)/len(self.Calificaciones)
+    
+    def mostrar_info(self):
+        print("---Datos del Alumno---")
+        print(f"Nombre:{self.Nombre}")
+        print(f"Materia:{self.Materia}")
+        print(f"Calificaciones:{self.Calificaciones}")
+        print(f"promedio{self.promedio():.2f}")
+
+if __name__ == "main":
+    Alumnos = []
+
+    while True:
+        print("---Menu de Calificaciones---")
+        print("1.Agregar Alumno")
+        print("2.Ver promedios")
+        print("3.Eliminar Alumno")
+        print("4.Buscar Alumno")
+        print("5.Salir")
+        opcion = input("Elige una opcion:  ")
+
+        if opcion == "1":
+            Nombre = input("Nombre del alumno: ")
+            Materia = input("Materia: ")
+            calificaciones = []
+
+            print("Ingresa 5 Calificaciones: ")
+            for i in range(5):
+                calificaciones = float(input(f"Calificacion {i+1}:"))
+                calificaciones.append(calificaciones) 
+
+                alumno = Alumno(Nombre,Materia,calificaciones)
+                Alumnos.append(alumno)
+                print("Alumno Agregado")
+
+        elif opcion == "2":
+            if not alumnos:
+                print("No hay Alumnos registrados")
+            else:
+                for alumno in Alumnos:
+                    alumno.mostrar_info()
+
+        elif opcion == "3":
+            if not Alumnos:
+                print("No hay alumnos para eliminar")
+            else:
+                Nombre_eliminar = input("Ingresa el alumno para eliminar: ")
+                encontrado = False
+                for alumno in Alumnos:
+                    if alumno.Nombre == Nombre_eliminar:
+                            Alumnos.remove(alumno)
+                            print(f"Alumno {Nombre_eliminar} eliminado correctamente.")
+                            encontrado = True
+                            break
+                    if not encontrado:
+                        print("Alumno no encontrado")
+
+        elif opcion == "4":
+
+
+
